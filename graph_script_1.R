63b496a68d4bc177076fda4674f007f3ccd2a4df
Michael Cattell <86273177+mjcj01@users.noreply.github.com>
2022-03-27 14:23:08 -0400

    Updated the graph appearance
    I removed the ticks and the names of the school districts along the x axis as well as the x axis gridlines

--- graph_script_1.R
+++ graph_script_1.R
@@ -3,6 +3,12 @@ library(tidyverse)
 median_cost_distance_graph <- expenditures_and_scores %>%
   mutate(school_districts = fct_reorder(school_districts, desc(distance_from_median_cost))) %>%
   ggplot(aes(x = school_districts, y = distance_from_median_cost)) +
-  geom_col()
+  geom_col() +
+  scale_y_continuous(limits = c(-5000,7500)) +
+  theme(
+    axis.ticks.x = element_blank(),
+    axis.text.x = element_blank(),
+    panel.grid.major.x = element_blank()
+  )
 
 median_cost_distance_graph
