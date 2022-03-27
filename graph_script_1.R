d0659053515819c8b8fd1809e3e98cbed411fb0d
Michael Cattell <86273177+mjcj01@users.noreply.github.com>
2022-03-27 16:52:00 -0400

    Update graph_script_1.R
    Added vertical line at median success cost

--- graph_script_1.R
+++ graph_script_1.R
@@ -4,6 +4,7 @@ median_cost_distance_graph <- expenditures_and_scores %>%
   mutate(school_districts = fct_reorder(school_districts, desc(distance_from_median_cost))) %>%
   ggplot(aes(x = school_districts, y = distance_from_median_cost)) +
   geom_col() +
+  geom_vline(xintercept = 188) +
   scale_y_continuous(limits = c(-5000,7500)) +
   theme(
     axis.ticks.x = element_blank(),
